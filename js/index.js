6a49dd19e711890e33f1e4cc349803a7f74bf0c5
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-05-03 15:19:31 -0400

    Display

--- js/index.js
+++ js/index.js
@@ -20,17 +20,35 @@ let outs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 let p = new Perceptron(4,new Relu());
 
-for(let i=0;i<inputs.length;i++)
+function change(number)
 {
-    console.log(i,p.getOutput(inputs[i]));
-}
+    let button = document.getElementById("b"+number);
+    if(button.style.backgroundColor=="rgb(66, 66, 66)")
+        button.style.backgroundColor="#afb2af";
+    else
+        button.style.backgroundColor="#424242";
 
-for(let i=0;i<5000;i++)
-{
-    p.learn(inputs,outs);
+    displayScreen();
 }
-
-for(let i=0;i<inputs.length;i++)
+function displayScreen()
 {
-    console.log(i,p.getOutput(inputs[i]));
+    let b=[document.getElementById("b1"),document.getElementById("b2"),
+        document.getElementById("b3"),document.getElementById("b4")];
+    let input=[0,0,0,0];
+    for(let i=0;i<b.length;i++)
+    {
+        if(b[i].style.backgroundColor=="rgb(66, 66, 66)")
+            input[i]=1;
+    }
+    let outputScreen=document.getElementById("answer");
+    outputScreen.innerHTML=""+Math.round(p.getOutput(input));
+}
+function learn()
+{
+    for(let i=0;i<5000;i++)
+    {
+        p.learn(inputs,outs);
+    }
+    displayScreen();
+
 }
